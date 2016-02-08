8c091f9dd3f57129ab85a18103a5ffed01576b6d
Jayanth Koushik <jkoushik@cs.cmu.edu>
2016-02-08 14:11:50 -0500

    Improve transcription feature extraction.

--- tools/extract_transc_feats.py
+++ tools/extract_transc_feats.py
@@ -1,9 +1,9 @@
 # coding=utf-8
+# extract_transc_feats.py: extract word features from transcriptions.
 
 from __future__ import print_function
 import glob
 import re
-import string
 import itertools
 
 import nltk
@@ -28,26 +28,27 @@ with open(TRANSC_FEATS_FILE, "w") as feats_file:
         with open(fname) as f:
             transc_str = f.read()
 
+        transc_str = transc_str.replace("\r\n", " ")
+        transc_str = transc_str.replace("\r\r", " ")
+        transc_str = transc_str.replace("\n", " ")
+        transc_str = transc_str.replace("-", " ")
+
         # Convert the transcription to lower case, but make
         # filler markers (like "umm", "uhh" etc.) upper case.
         transc_str = transc_str.lower()
         m = re.findall("[{(]([^\s]*?)[)}]", transc_str)
         for word in m:
             transc_str = re.sub("[({{]{}[)}}]".format(word),
-                                word.upper(), transc_str)
+                                " " + word.upper() + " ", transc_str)
 
         # Construct the feature vector.
         toks = nltk.word_tokenize(transc_str)
         feat_vec = []
         for tok in toks:
-            if not tok in vocab:
-                if not all(c in string.ascii_lowercase for c in tok):
-                    # Learn the special words.
-                    vocab[tok] = len(vocab)
             if tok in vocab:
                 feat_vec.append(vocab[tok])
             else:
-                feat_vec.append(-1)
+                feat_vec.append(len(vocab))
 
         if len(feat_vec) > max_feat_vec_len:
             max_feat_vec_len = len(feat_vec)
