66fdec7a68c7ddcca684378ddc56f4ddbd498569
Jayanth Koushik <jkoushik@cs.cmu.edu>
2016-02-16 23:21:46 -0500

    Make fixes to univerb.

--- src/univerb.py
+++ src/univerb.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import sys
 import argparse
 import random
+import os
 from collections import defaultdict
 
 import numpy as np
@@ -18,6 +19,7 @@ from sklearn.cross_validation import KFold
 
 
 FEATS_FILE = "data/feats/transc.txt"
+SAVE_PATH = "data/saves/univerb/"
 EMBEDDING_SIZE = 256
 HIDDEN_LAYER_SIZE = 256
 DROPOUT_PROB = 0.5
@@ -25,7 +27,10 @@ MAX_FEATS = 1000
 DEFAULT_EPOCHS = 1
 DEFAULT_BATCH_SIZE = 100
 VALIDATION = 10
-VOCAB_SIZE = 7986
+VOCAB_SIZE = 7987
+
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("--epochs", type=int, default=DEFAULT_EPOCHS)
@@ -72,7 +77,11 @@ def generate_XY(idx, speakers, speaker_data_map):
 speakers = speaker_data_map.keys()
 random.shuffle(speakers)
 accs = []
+fold = 0
+init_weights = model.get_weights()
 for train_idx, test_idx in KFold(len(speakers), VALIDATION):
+    fold += 1
+    print("FOLD {}".format(fold))
     X_train, y_train = generate_XY(train_idx, speakers, speaker_data_map)
     X_test, y_test = generate_XY(test_idx, speakers, speaker_data_map)
 
@@ -81,14 +90,24 @@ for train_idx, test_idx in KFold(len(speakers), VALIDATION):
     X_test = sequence.pad_sequences(X_test, maxlen=MAX_FEATS, padding="post",
                                     truncating="post")
 
-    model.fit(X_train, y_train, batch_size=args.batch_size, nb_epoch=args.epochs,
-              show_accuracy=True)
+    model.set_weights(init_weights)
+    model.reset_states()
+    history = model.fit(X_train, y_train, batch_size=args.batch_size,
+                        nb_epoch=args.epochs, show_accuracy=True)
 
     _, acc = model.evaluate(X_test, y_test, batch_size=args.batch_size,
                             show_accuracy=True)
     accs.append(acc)
-    print("Test accuracy: {}".format(acc))
+    print("Fold {} test accuracy: {}".format(fold, acc))
 
+    model.save_weights(os.path.join(SAVE_PATH,
+                                    "fold{}_weights.h5".format(fold)),
+                       overwrite=True)
+    print("\n".join(map(str, history.history["acc"])),
+          file=open(os.path.join(SAVE_PATH, "fold{}_accs.txt".format(fold)), "w"))
+
+print("\n".join(map(str, accs)),
+      file=open(os.path.join(SAVE_PATH, "accs.txt"), "w"))
 final_acc = sum(accs) / VALIDATION
 print("Final accuracy: {}".format(final_acc))
 
